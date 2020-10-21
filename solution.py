b558b9f2b6e93e2f111acdc70de27d574db77fb1
EC2 Default User <ec2-user@ip-172-31-28-6.us-east-2.compute.internal>
2020-10-21 15:30:31 +0000

    Minor changes, some comments

--- solution.py
+++ solution.py
@@ -1,19 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask import request
   
 app = Flask(__name__)
 
 def process(content):
-	print("The type of parsed JSON is: ", type(content['payload']))
-	print("The type of individual show is: ", type(content['payload'][0]))
 	results = []
-	count = 0
+	
 	for show in content['payload']:
-		if 'country' in show:
+		if 'country' in show: #Additional check as 1 of the sample JSON was of a different format
 			if show['drm'] == True and show['episodeCount'] > 0:
 				results.append({"image": show['image']['showImage'], "slug": show['slug'], "title": show['title']})
-				#print("image: " , show['image']['showImage'], "; slug: ", show['slug'], "; title", show['title'])
-
+	
 	return {"response": results}
 
 
@@ -22,12 +19,12 @@ def process(content):
 @app.route('/', methods = ['POST'])
 def postJsonHandler():
     if not request.is_json:
-    	return {"response": "Could not decode request: JSON parsing failed"}
+    	return jsonify({"response": "Could not decode request: JSON parsing failed"})
     content = request.get_json()
-    return process(content)
+    return jsonify(process(content))
     
-
+# This method is to just check if the site is live
 @app.route('/hello', methods = ['GET'])
 def index():
 	return ''' <h2> Hello! This site parses json and returns stuff on the main page. </h2>  '''  
-app.run(host='127.0.0.1', port= 8090) # host='0.0.0.0', port= 443
+app.run(host='0.0.0.0', port= 80) 
